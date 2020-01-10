a0dd378ac99e55eae4812d0212af8a8770f14e16
Brock Friedrich <brocklfriedrich@gmail.com>
2020-01-09 21:30:07 -0600

    cleanup

--- src/fracx/manage.py
+++ src/fracx/manage.py
@@ -10,17 +10,12 @@ from flask.cli import FlaskGroup, AppGroup
 
 from fracx import create_app, db
 from config import get_active_config
-import util
 
 logger = logging.getLogger()
 
 
 CONTEXT_SETTINGS = dict(
     help_option_names=["-h", "--help"], ignore_unknown_options=False
-)
-STATUS_COLOR_MAP = defaultdict(
-    lambda: "white",
-    {"success": "green", "error": "red", "timeout": "yellow", "failed": "red"},
 )
 
 conf = get_active_config()
@@ -36,15 +31,6 @@ def get_terminal_columns():
 
 def hr():
     return "-" * get_terminal_columns()
-
-
-# @cli.command("urlmap")
-# def urlmap():
-#     """Prints out all routes"""
-#     click.echo("{:50s} {:40s} {}".format("Endpoint", "Methods", "Route"))
-#     for route in app.url_map.iter_rules():
-#         methods = ",".join(route.methods)
-#         click.echo("{:50s} {:40s} {}".format(route.endpoint, methods, route))
 
 
 @cli.command()
@@ -125,26 +111,12 @@ def collector(update_on_conflict, ignore_on_conflict, use_existing):
     ftp.cleanup()
 
 
-@run_cli.command(context_settings=dict(ignore_unknown_options=True))
-@click.argument("args", nargs=-1, type=click.UNPROCESSED)
-def web(args):
-    cmd = ["gunicorn", "wsgi"] + list(args)
-    subprocess.call(cmd)
-
-
 @cli.command()
 def endpoints():
     from collector import Endpoint
 
     for name, ep in Endpoint.load_from_config(conf).items():
         click.secho(name)
-
-
-@cli.command()
-def recreate_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
 
 
 def main(argv=sys.argv):
@@ -160,30 +132,6 @@ def main(argv=sys.argv):
     return 0
 
 
-# @cli.command()
-# def test():
-#     """Runs the tests without code coverage"""
-#     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
-#     result = unittest.TextTestRunner(verbosity=2).run(tests)
-#     if result.wasSuccessful():
-#         return 0
-#     sys.exit(result)
-
-
-# @cli.command()
-# def cov():
-#     """Runs the unit tests with coverage."""
-#     tests = unittest.TestLoader().discover('project/tests')
-#     result = unittest.TextTestRunner(verbosity=2).run(tests)
-#     if result.wasSuccessful():
-#         COV.stop()
-#         COV.save()
-#         print('Coverage Summary:')
-#         COV.report()
-#         COV.html_report()
-#         COV.erase()
-#         return 0
-#     sys.exit(result)
 cli.add_command(run_cli)
 
 if __name__ == "__main__":
